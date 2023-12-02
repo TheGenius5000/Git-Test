786ef4c82192d68c38fe361df947d4cc509b06be
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-02 17:55:02 +0000

    First template

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World!")
+    return render(request, 'home/index.html', {'today': datetime.today()})
