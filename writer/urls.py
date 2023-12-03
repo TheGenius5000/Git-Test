eeb8cc93b26a43f7d1e2c11329b6701f3066ad37
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-03 20:39:24 +0000

    POST test

--- writer/urls.py
+++ writer/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('writer', views.WriterListView.as_view()),
-    path('writer/<int:pk>', views.WriterDetailView.as_view())
+    path('writer', views.WriterListView.as_view(), name="writer.list"),
+    path('writer/<int:pk>', views.WriterDetailView.as_view(), name="writer.detail"),
+    path('writer/new', views.WriterCreateView.as_view(), name="writer.new"),
 ]
--- writer/views.py
+++ writer/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView
 
 from .models import Notes
 
 
 # Create your views here.
+
+class WriterCreateView(CreateView):
+    model = Notes
+    fields = ['title', 'text']
+    success_url = '/smart/writer'
+    template_name = "writer/write-form.html"
+
 
 class WriterListView(ListView):
     model = Notes
