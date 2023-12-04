ac9bc7c3aaaad2e660566232252001f2f1db171c
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-04 16:56:03 +0000

    Foreign key user insertion w/form

--- writer/views.py
+++ writer/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http.response import Http404, HttpResponseRedirect
 from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Notes
 from .forms import WriterForm
 
@@ -11,6 +13,7 @@ from .forms import WriterForm
 class WriterDeleteView(DeleteView):
     model = Notes
     success_url = '/smart/writer'
+
 
 class WriterUpdateView(UpdateView):
     model = Notes
@@ -23,12 +26,21 @@ class WriterCreateView(CreateView):
     success_url = '/smart/writer'
     form_class = WriterForm
 
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
-class WriterListView(ListView):
+
+class WriterListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "writer/notes_list.html"
+    login_url = "/admin"
 
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class WriterDetailView(DetailView):
     model = Notes
